f744e04b5a249d51f674ea740e7f8cbd27566635
kana <nicht@s8.xrea.com>
2009-07-11 18:21:46 +0900

    Learn about layout-by-code and layout-by-resource

--- src/net/whileimautomaton/yunoroid/Yunoroid.java
+++ src/net/whileimautomaton/yunoroid/Yunoroid.java
@@ -2,6 +2,7 @@ package net.whileimautomaton.yunoroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Yunoroid extends Activity
 {
@@ -10,6 +11,15 @@ public class Yunoroid extends Activity
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+
+		if (false) {
+			// Layout by code.
+			TextView tv = new TextView(this);
+			tv.setText("Yunoroid @ layout by code");
+			setContentView(tv);
+		} else {
+			// Layout by resource.
+			setContentView(R.layout.main);
+		}
 	}
 }
